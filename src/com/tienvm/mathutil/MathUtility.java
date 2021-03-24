2277964269d31acf64bd28b256d39e1db4652974
tienvmt <tienvmt02@gmail.com>
2021-03-24 10:10:02 +0700

    Update code and GREEN 4EVER

--- src/com/tienvm/mathutil/MathUtility.java
+++ src/com/tienvm/mathutil/MathUtility.java
@@ -29,7 +29,7 @@ public class MathUtility {
         if(n == 0 || n == 1)
             return 1;
         //CPU chạy đến đây sure n = 2...20
-        long result = 0; 
+        long result = 1; 
         for (int i = 2; i <= n; i++) 
             result *= i;
         return result;
